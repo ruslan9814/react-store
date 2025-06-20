e42341b3eca3376d63bea0552170e381d06b0fcc
ruslan9814 <167958368+ruslan9814@users.noreply.github.com>
2025-06-20 17:07:05 +0400

    Update vite config and rebuild project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // относительные пути, чтобы не было проблем с загрузкой ресурсов
   plugins: [react()],
+  build: {
+    outDir: 'dist', // папка с билдом
+  },
 })
