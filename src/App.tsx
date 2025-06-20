dc47ae46f842febc6304090f7ab9bdb09367930d
ruslan9814 <167958368+ruslan9814@users.noreply.github.com>
2025-06-20 18:50:30 +0400

    Test simple App Loaded

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,8 @@ import DeleteProduct from './pages/DeleteProduct';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
- 
-<Routes>
-  <Route path="/" element={<ProductList />} />
-  <Route path="/product" element={<ProductList />} />
-  <Route path="/add-product" element={<AddProduct />} />
-  <Route path="/product/:id" element={<ProductDetailPage />} />  {/* детали */}
-  <Route path="/edit-product/:id" element={<EditProduct />} />   {/* редактирование */}
-  <Route path="/delete-product/:id" element={<DeleteProduct />} /> {/* удаление */}
-</Routes>
-
-
- 
-    </Router>
-  );
+  return <h1>App Loaded</h1>;
 }
+
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // относительные пути, чтобы не было проблем с загрузкой ресурсов
+  base: '/', // или '/название-папки/' если деплоишь в поддиректорию
   plugins: [react()],
   build: {
-    outDir: 'dist', // папка с билдом
+    outDir: 'dist',
   },
- 
 })
